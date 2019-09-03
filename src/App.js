4060a9aca64154742776abbc0b223611f864192a
Rejohnbd <rejohn.cse@gmail.com>
2019-09-03 14:57:48 +0600

    Add Functional Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import Contact from './components/Contact';
 
 import './App.css';
@@ -6,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>The App Component</h1>
+      <Header />
       <Contact />
       <Contact />
     </div>
