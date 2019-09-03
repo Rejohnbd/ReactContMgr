a9dd2985560f85d8963ede4d0df6d28f9f7c012d
Rejohnbd <rejohn.cse@gmail.com>
2019-09-03 14:19:29 +0600

    Create React App and Create Class Component

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Contact extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <h4>Rejohn</h4>
+                <ul>
+                    <li>Email: rejohn@gmail.com</li>
+                    <li>Phone: 01717546533</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default Contact;
