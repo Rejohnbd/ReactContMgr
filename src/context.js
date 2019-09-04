a21401c1399bc45cd97de555abaf26b1950955d4
Rejohnbd <rejohn.cse@gmail.com>
2019-09-04 14:44:05 +0600

    Use axios and HTTP GET Request in Context

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -21,30 +22,16 @@ const reducer = (state, action) => {
 
 export class Provider extends Component{
     state = {
-        contacts: [
-            {
-                id: 1,
-                name: 'Rejohn',
-                email: 'rejohn@gmail.com',
-                phone: '01717546533'
-            },
-            {
-                id: 2,
-                name: 'Atik',
-                email: 'atik@gmail.com',
-                phone: '01552607608'
-            },
-            {
-                id: 3,
-                name: 'Rajon',
-                email: 'rajon@gmail.com',
-                phone: '01741063214'
-            }
-        ],
+        contacts: [],
         dispatch: (action) => {
             this.setState( state => reducer(state, action) )
         }
     };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => this.setState({ contacts: res.data }))
+    }
 
     render() {
         return (
